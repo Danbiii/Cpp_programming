a708b9f8a09160f7427b9eb5b756155a6ec51812
danbi <daanv319@gmail.com>
2020-10-13 19:40:29 +0900

    test of dtype in accordance with dtype size

---
+++ study/dtype_test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int x = 10; // 10 = 10진수, 십진수로 10
+	int y = 010; // 010 = 8진수, 십진수로 8
+	int z = 0x10; // 0x10 = 16진수, 십진수로 16
+
+	cout << "x = " << x << endl;
+	cout << "y = " << y << endl;
+	cout << "z = " << z << endl;
+
+	return 0;
+}
