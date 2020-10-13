3851a47f7d0c017eeea3617b9d655d8859c95ae7
danbi <daanv319@gmail.com>
2020-10-13 17:20:55 +0900

    test of dtype using 'int'

---
+++ study/dtype.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+	int var1; int var2;
+
+	var1 = 1;
+	var2 = 65;
+
+	var1 = var1 + var2;
+	cout << "var1 = " << var1 << endl;
+	
+	return 0;
+}
