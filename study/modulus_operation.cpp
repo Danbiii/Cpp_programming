9b5f72a6c9938581e95cdeaa2870b87ad3916d7a
danbi <daanv319@gmail.com>
2020-10-29 21:30:03 +0900

    test of modulus operation with second and minute

---
+++ study/modulus_operation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int input, minute, second;
+	const int SEC_PER_MINUTE = 60;
+	
+	cout << "초 단위의 시간을 입력하시오: ";
+	cin >> input;
+
+	minute = input/SEC_PER_MINUTE; //몇분
+	second = input % SEC_PER_MINUTE; //몇초
+
+	cout << input << "초는" << minute << "분" << second << "초입니다." << endl;
+
+	return 0;
+}
